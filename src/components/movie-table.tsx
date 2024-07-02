eb816faeacbd697bf92bdfed13a4999d8183030f
partyparrotgreg <2233348+partyparrotgreg@users.noreply.github.com>
2024-07-02 19:01:51 +0100

    fixing styles

--- src/components/movie-table.tsx
+++ src/components/movie-table.tsx
@@ -3,13 +3,12 @@
 import { movies as defaultMovies } from "@/data/movies";
 import { cn } from "@/lib/utils";
 import { fetchMovieData } from "@/services";
-import { CheckCircle } from "lucide-react";
+import { CheckCircle, SearchIcon } from "lucide-react";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { Badge } from "./ui/badge";
-import { Input } from "./ui/input";
-import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
+import { Button } from "./ui/button";
 
 type Movie = {
   title: string;
@@ -30,6 +29,7 @@ export const MovieTable = () => {
   const [movieList, setMovieList] = useState<Movies>([]);
   const [filter, setFilter] = useState<Filter>("all");
   const [sort, setSort] = useState<Sort>("rating");
+  const [search, setSearch] = useState<string>("");
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -65,28 +65,6 @@ export const MovieTable = () => {
     localStorage.setItem("movieList", JSON.stringify(newMovieList));
   };
 
-  console.log({ movieList });
-
-  const sortByRating = (a: Movie, b: Movie) => {
-    if (a.rating < b.rating) {
-      return 1;
-    }
-    if (a.rating > b.rating) {
-      return -1;
-    }
-    return 0;
-  };
-
-  const sortByYear = (a: Movie, b: Movie) => {
-    if (a.year < b.year) {
-      return 1;
-    }
-    if (a.year > b.year) {
-      return -1;
-    }
-    return 0;
-  };
-
   const handleFilterChange = (newFilter: Filter) => {
     setFilter(newFilter);
   };
@@ -97,8 +75,15 @@ export const MovieTable = () => {
 
   return (
     <div className="flex flex-col gap-6">
-      <div className="flex flex-row gap-4 justify-between items-center">
-        <Input placeholder="Search" />
+      {/* <div className="flex flex-row gap-4 justify-between items-center">
+        <Input
+          placeholder="Search"
+          onChange={(event: ChangeEvent<HTMLInputElement>) => {
+            const search = event.target.value;
+            setSearch(search);
+          }}
+        />
+
         <Tabs defaultValue={filter}>
           <TabsList>
             {filters.map((filter, index) => (
@@ -125,57 +110,70 @@ export const MovieTable = () => {
             ))}
           </TabsList>
         </Tabs>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 3xl:grid-cols-7  gap-6 2xl:gap-12">
+      </div> */}
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 3xl:grid-cols-6  gap-6 2xl:gap-12">
         {movieList.map((movie, index) => (
           <div
             key={index}
             onClick={() => handleCheckboxChange(index)}
             className={cn(
-              movie.seen
-                ? "bg-green-500 text-background border-4 border-green-500"
-                : "bg-card",
-              "flex items-start  rounded-2xl flex-col relative overflow-hidden cursor-pointer"
+              movie.seen ? "bg-green-500 text-background " : "bg-card",
+              "flex items-start  rounded-2xl flex-row relative overflow-hidden cursor-pointer"
             )}
           >
             <div className="text-md font-semibold py-2 px-3 bg-yellow-400 absolute left-3 top-0 rounded-b-2xl text-black">
               {movie.rating}
             </div>
 
-            <Image
-              src={movie.poster}
-              alt={movie.title}
-              width={400}
-              height={600}
-              className="rounded-2xl aspect-[10/15] object-cover"
-            />
-            <div className="p-4">
-              <h3
-                className="text-lg font-bold"
-                onClick={async (e: React.MouseEvent<HTMLElement>) => {
+            <div className="rounded-2xl aspect-[10/15] relative h-full w-1/2">
+              <Image
+                src={movie.poster}
+                alt={movie.title}
+                width={400}
+                height={600}
+                className="object-cover w-auto h-full"
+              />
+            </div>
+            <div className="p-4 flex flex-col gap-2 justify-between w-1/2 h-full">
+              <Badge
+                className="absolute right-2 top-2"
+                variant={movie.seen ? "success" : "secondary"}
+              >
+                {movie.seen ? "Seen" : "Unseen"}
+                {movie.seen && <CheckCircle size={16} className="ml-2" />}
+              </Badge>
+              <div className="grow flex flex-col gap-2">
+                <h3
+                  className="text-lg font-bold mt-8 leading-5"
+                  onClick={async (e: React.MouseEvent<HTMLElement>) => {
+                    e.stopPropagation();
+                    try {
+                      await navigator.clipboard.writeText(movie.title);
+                    } catch (error) {
+                      console.error("Failed to copy text to clipboard:", error);
+                    } finally {
+                      toast.success(`"${movie.title}" copied to clipboard`);
+                    }
+                  }}
+                >
+                  {movie.title}
+                </h3>
+                <p className="text-sm">{movie.year}</p>
+                <p className="text-sm opacity-50">{movie.genre}</p>
+              </div>
+              <Button
+                size={"icon"}
+                variant={"ghost"}
+                onClick={(e: React.MouseEvent<HTMLElement>) => {
                   e.stopPropagation();
-                  try {
-                    await navigator.clipboard.writeText(movie.title);
-                  } catch (error) {
-                    console.error("Failed to copy text to clipboard:", error);
-                  } finally {
-                    toast.success(`"${movie.title}" copied to clipboard`);
-                  }
+                  console.log("Searching for " + movie.title);
+                  const encodeTitle = encodeURIComponent(movie.title);
+                  const googleSearchUrl = `https://www.google.com/search?q=${encodeTitle}`;
+                  window.open(googleSearchUrl, "_blank");
                 }}
               >
-                {movie.title}
-              </h3>
-              <p className="text-sm">{movie.year}</p>
-              <p className="text-sm opacity-50">{movie.genre}</p>
-            </div>
-            <div className="p-2 rounded-lg absolute right-0 top-0">
-              {/* Badge with seen/unseen */}
-              <div className="flex items-center gap-2 p-2 rounded-lg absolute right-0 top-0">
-                <Badge variant={movie.seen ? "success" : "secondary"}>
-                  {movie.seen ? "Seen" : "Unseen"}
-                  {movie.seen && <CheckCircle size={16} className="ml-2" />}
-                </Badge>
-              </div>
+                <SearchIcon className="h-5 w-5" />
+              </Button>
             </div>
           </div>
         ))}
